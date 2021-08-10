cd0c54234894550985a8702a4c9fecc4c42e8db0
David Hoppenbrouwers <david@salt-inc.org>
2021-08-10 12:42:18 +0200

    Remove useless CSR listing

--- kernel/src/arch/riscv/trap.s
+++ kernel/src/arch/riscv/trap.s
@@ -4,75 +4,6 @@
 
 #.include	"registers.s"
 
-.equ		USTATUS,		0x000
-.equ		UIE,			0x004
-.equ		UTVEC,			0x005
-
-.equ		USCRATCH,		0x040
-.equ		UEPC,			0x041
-.equ		UCAUSE,			0x042
-.equ		UTVAL,			0x043
-.equ		UIP,			0x044
-
-.equ		FFLAGS,			0x001
-.equ		FRM,			0x002
-.equ		FCSR,			0x003
-
-.equ		CYCLE,			0xc00
-.equ		TIME,			0xc01
-.equ		INSTRET,		0xc02
-
-.equ		CYCLEH,			0xc80
-.equ		TIMEH,			0xc81
-.equ		INSTRETH,		0xc82
-
-.equ		SSTATUS,		0x100
-.equ		SEDELEG,		0x102
-.equ		SIDELEG,		0x103
-.equ		SIE,			0x104
-.equ		STVEC,			0x105
-.equ		SCOUNTEREN,		0x106
-
-.equ		SSCRATCH,		0x140
-.equ		SEPC,			0x141
-.equ		SCAUSE,			0x142
-.equ		STVAL,			0x143
-.equ		SIP,			0x144
-
-.equ		SATP,			0x180
-
-.equ		MVENDORID,		0xf11
-.equ		MARCHID,		0xf12
-.equ		MIMP,			0xf13
-.equ		MHARTID,		0xf14
-
-.equ		MSTATUS,		0x300
-.equ		MISA,			0x301
-.equ		MEDELEG,		0x302
-.equ		MIDELEG,		0x303
-.equ		MIE,			0x304
-.equ		MTVEC,			0x305
-.equ		MCOUNTEREN,		0x306
-
-.equ		MSCRATCH,		0x340
-.equ		MEPC,			0x341
-.equ		MCAUSE,			0x342
-.equ		MTVAL,			0x343
-.equ		MIP,			0x344
-
-.equ		MCYCLE,			0xb00
-.equ		MINSTRET,		0xb02
-
-.equ		MCYCLEH,		0xb80
-.equ		MINSTRETH,		0xb02
-
-.equ		MCOUNTINHIBIT,	0x320
-
-.equ		TSELECT,		0x7a0
-
-.equ		DCSR,			0x7b0
-.equ		DPC,			0x7b1
-
 .equ		SYSCALL_MAX,			16
 .equ		SYSCALL_ERR_NOCALL, 	1
 
@@ -243,10 +174,10 @@ trap_handler:
 	# An untested attempt at catering to pipelining
 	la		x28, sync_trap_table		# jmp
 	ld		sp, REGSTATE_SIZE + 0 * REGBYTES (x31)		# stack pointer
-	csrr	x29, SCAUSE					# jmp
+	csrr	x29, scause					# jmp
 	sd		x30, 30 * REGBYTES (x31)	# store A
 	slli	x29, x29, 2					# jmp
-	csrrw	x30, SSCRATCH, x31			# store B	(restores original mscratch)
+	csrrw	x30, sscratch, x31			# store B	(restores original mscratch)
 	add		x28, x28, x29				# jmp
 	sd		x30, 31 * REGBYTES (x31)	# store B
 	# Execute the appropriate routine
@@ -301,9 +232,9 @@ trap_syscall:
 
 	# Skip the ecall instruction, which is always 4 bytes long (there is no
 	# compressed version of it).
-	csrr	t0, SEPC
+	csrr	t0, sepc
 	addi	t0, t0, 4
-	csrw	SEPC, t0
+	csrw	sepc, t0
 
 	# Check if the syscall exists, otherwise return the 'no syscall' error code
 	li		t1, SYSCALL_MAX
@@ -324,7 +255,7 @@ trap_syscall:
 
 	# Restore all integer registers except a0 and a1, then return
 0:
-	csrr	x31, SSCRATCH
+	csrr	x31, sscratch
 	ld		x1, 1 * REGBYTES (x31)
 	ld		x2, 2 * REGBYTES (x31)
 	ld		x3, 3 * REGBYTES (x31)
@@ -467,16 +398,16 @@ trap_start_task:
 	sd		t0, 0(t1)
 	# Switch to U-mode when executing mret.
 	li		t0, 0 << 11
-	csrw	SSTATUS, t0
+	csrw	sstatus, t0
 	# Set up the VMS.
 	ld		t0, REGSTATE_SIZE + 1 * REGBYTES (a0)
-	csrw	SATP, t0
+	csrw	satp, t0
 	sfence.vma
 	# Setup the scratch register.
-	csrw	SSCRATCH, a0
+	csrw	sscratch, a0
 	# Restore the program counter
 	ld		t0, 0 * REGBYTES (a0)
-	csrw	SEPC, t0
+	csrw	sepc, t0
 	# Restore all float registers
 	load_float_registers	a0
 	# Restore all integer registers
